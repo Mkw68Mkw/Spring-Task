6544232456befb561fc25cae9b4ece74310422b4
Mkw68Mkw <mkw68mkw@outlook.de>
2025-03-23 20:51:49 +0100

    env fehler behoben

--- src/environment.ts
+++ src/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080/api' // Für lokales Backend
+    apiUrl: 'https://ebsspring.mkw68.com/api' // Für lokales Backend
   };
